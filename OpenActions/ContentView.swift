dd0ca54043a27da52196266ad7b224f221b2b912
Pierre Abi-aad <pierre.abi-aad@adevinta.com>
2022-01-03 09:13:55 +0100

    feat: design home screen app

--- OpenActions/ContentView.swift
+++ OpenActions/ContentView.swift
@@ -8,9 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @Environment(\.openURL) var openURL
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            Spacer()
+            VStack(alignment: .center, spacing: 24, content: {
+                Spacer()
+                Image("logo").frame(width: 100, height: 100, alignment: .center)
+                Text("Open you Shortcuts app to use actions from\nOpen Actions")
+                    .font(.body)
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(Color.white)
+                Spacer()
+                Button("Contribute on GitHub") {
+                    guard let url = URL(string: "https://github.com/abiaad/OpenActions")
+                    else {
+                        return
+                    }
+                    self.openURL(url)
+                }
+                .foregroundColor(Color.white)
+                .padding()
+                .background(Color(.init(red: 20/255, green: 20/255, blue: 20/255, alpha: 1)))
+                .cornerRadius(8)
+                Spacer()
+            })
+            Spacer()
+        }
+        .background(Color.black)
     }
 }
 
