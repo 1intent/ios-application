d626f63e67c85f82e453032084c078089d78869f
Pierre Abi-aad <abiaad.pierre@me.com>
2022-02-13 18:27:22 +0100

    fix: use 1Intent instead of Open Actions in home page's app

--- Packages/MainApplication/Tests/MainApplicationTests/Presentation/ViewModels/HomeViewModelTests.swift
+++ Packages/MainApplication/Tests/MainApplicationTests/Presentation/ViewModels/HomeViewModelTests.swift
@@ -15,7 +15,7 @@ final class HomeViewModelTests: XCTestCase {
         let viewModel = HomeViewModel()
 
         //WHEN...THEN
-        XCTAssertEqual(viewModel.title, "Open you Shortcuts app to use actions from\nOpen Actions", "wrong title")
+        XCTAssertEqual(viewModel.title, "Open you Shortcuts app to use actions from\n1Intent", "wrong title")
         XCTAssertEqual(viewModel.contributionTitle, "Contribute on Github", "wrong contributionTitle")
         XCTAssertEqual(viewModel.logoImageName, "logo", "wrong logoImageName")
     }
