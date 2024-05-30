af2581d0763bb97d1142dcb36d6742451d78fdd3
Pierre Abi-aad <pierre.abi-aad@adevinta.com>
2024-05-30 17:39:16 +0200

    feat: add TimeIntervalToDateHandler

---
+++ IntentsExtension/Handlers/TimeIntervalToDate/TimeIntervalToDateHandler.swift
@@ -0,0 +1,21 @@
+//
+//  TimeIntervalToDateHandler.swift
+//  IntentsExtension
+//
+//  Created by Pierre Abi-aad on 30/05/2024.
+//
+
+import Foundation
+import AppIntents
+
+struct ConvertTimeIntervalToDateIntent: AppIntent {
+    static var title: LocalizedStringResource = "Convert Time Interval to Date"
+
+    @Parameter(title: "Time Interval", description: "Time interval in seconds")
+    var timeInterval: Double
+
+    func perform() async throws -> some ReturnsValue<Date> {
+        let date = Date(timeIntervalSince1970: self.timeInterval)
+        return .result(value: date)
+    }
+}
